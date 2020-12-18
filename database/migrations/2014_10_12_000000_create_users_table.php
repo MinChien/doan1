3562d772e81fb3078139c4edb97b7d84d10c22b9
Minh Chien <trinhminhminhchienchien@gmail.com>
2020-12-18 14:49:05 +0700

    Update 2014_10_12_000000_create_users_table.php

--- database/migrations/2014_10_12_000000_create_users_table.php
+++ database/migrations/2014_10_12_000000_create_users_table.php
@@ -14,8 +14,6 @@ class CreateUsersTable extends Migration
     public function up()
     {
         Schema::create('users', function (Blueprint $table) {
-            $table->charset = 'utf8';
-            $table->collation = 'utf8_unicode_ci';
             $table->id();
             $table->string('name');
             $table->string('email')->unique();
